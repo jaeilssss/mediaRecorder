c272a76284fc8c7e60ec376e5134ab1fd63b6388
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-17 23:59:44 +0900

    gg

--- app/src/main/java/com/example/mediarecorder/MainActivity.kt
+++ app/src/main/java/com/example/mediarecorder/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mediarecorder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
+
+    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private val recordButton : RecordButton by lazy {
         findViewById<RecordButton>(R.id.recordButton)
     }
@@ -14,11 +19,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        requestPermission()
         initViews()
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted = requestCode== REQUEST_RECORD_AUDIO_PERMISSION
+                && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted){
+            finish()
+        }
+    }
+
+    private fun requestPermission(){
+        requestPermissions(requiredPermissions, REQUEST_RECORD_AUDIO_PERMISSION)
+    }
   private fun initViews(){
         recordButton.updateIconWithState(state)
+    }
+
+    companion object{
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
