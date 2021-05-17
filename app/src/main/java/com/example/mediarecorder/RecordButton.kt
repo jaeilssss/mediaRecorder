37df4fb93eddad697405ae341f6ae124c7bb1c29
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-17 11:49:12 +0900

    5.17

---
+++ app/src/main/java/com/example/mediarecorder/RecordButton.kt
@@ -0,0 +1,32 @@
+package com.example.mediarecorder
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatImageButton
+import com.example.mediarecorder.State.*
+
+class RecordButton(
+
+    context : Context,
+    attrs : AttributeSet
+) : AppCompatImageButton(context, attrs){
+
+    fun updateIconWithState(state: State){
+        when(state){
+          BEFORE_RECORDING ->{
+                setImageResource(R.drawable.ic_record)
+            }
+            ON_RECORDING -> {
+
+                setImageResource(R.drawable.ic_stop24)
+            }
+            AFTER_RECORDING -> {
+                setImageResource(R.drawable.ic_play)
+            }
+            ON_PLAYING -> {
+                setImageResource(R.drawable.ic_stop24)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mediarecorder/State.kt
@@ -0,0 +1,9 @@
+package com.example.mediarecorder
+
+enum class State {
+
+    BEFORE_RECORDING,
+    ON_RECORDING,
+    AFTER_RECORDING,
+    ON_PLAYING
+}
